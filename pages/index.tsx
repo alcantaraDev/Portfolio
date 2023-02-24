49d070e4f3b09f3b0e0abcb7e53702f15a25fdc8
Isaque Alcantara <alcantara.isaque167@gmail.com>
2023-02-24 12:27:01 -0300

    update: insatall Next.js, talwindcss

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import { HeadSection } from "@/components/sections/head";
+
+export default function Home() {
+  return (
+    <>
+      <HeadSection/>
+      <main>
+      </main>
+    </>
+  )
+}
