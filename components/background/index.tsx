a458185e736d6af97922ef763f042a01b8f14ba8
Isaque Alcantara <alcantara.isaque167@gmail.com>
2023-02-24 12:35:44 -0300

    create: background

---
+++ components/background/index.tsx
@@ -0,0 +1,12 @@
+export function BackGround() {
+    return (
+        <div 
+        className="
+            bg-gradient-to-r from-[#1F1F1F] to-[#111111]
+            w-screen h-full
+            absolute top-0 left-0 z-[-1] 
+        "
+        >
+        </div>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
+import { BackGround } from "@/components/background";
 import { HeadSection } from "@/components/sections/head";
 
 export default function Home() {
   return (
     <>
       <HeadSection/>
+      <BackGround/>
       <main>
       </main>
     </>
