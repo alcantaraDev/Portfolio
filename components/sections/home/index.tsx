56d75944424aed4147bbc43b61b20c2f7dd78850
Isaque Alcantara <alcantara.isaque167@gmail.com>
2023-05-11 18:49:03 -0300

    updated new home page layout

--- components/sections/home/index.tsx
+++ components/sections/home/index.tsx
@@ -1,42 +1,21 @@
-import { SlideIn } from "@/components/animate";
-import { GradientText } from "@/components/gradientText";
 import { Link } from "@/components/link";
 
 export function HomeSection() {
     return (
-        <section id="home"
+        <section 
+        id="home" 
         className="
-        w-screen h-[calc(100vh-80px)]
-        flex flex-col-reverse lg:flex-row justify-center items-center gap-[20px] lg:gap-[100px]
+        text-white bg-[url('/home-bg.png')] bg-center bg-cover
+        flex flex-col justify-center items-center gap-10
+        h-screen
         "
         >
-            <SlideIn direction="leftToRight" delay={400}>
-                <div className="flex flex-col gap-5">
-                    <h1 
-                    className="
-                    text-white font-bold
-                    text-[12vw] md:text-[8vw]  lg:text-5xl 2xl:text-[3vw]
-                    "
-                    >
-                        <GradientText className=" text-[1.5em]">Olá!</GradientText>
-                        Sou Isaque <br/> Alcântara
-                    </h1>
-                    <Link 
-                    className="w-fit" 
-                    href="#" 
-                    type="button">
-                        Conheça meu trabalho
-                    </Link>
-                </div>
-            </SlideIn>
-            <SlideIn direction="rightToLeft" delay={400}>
-                <img 
-                className="
-                hidden lg:block
-                w-[25vw]
-                " 
-                src="/home-cover.png"/>
-            </SlideIn>
+            <h1 className="text-6xl text-center font-bold w-[478px]">
+                Precisa de um Designer?
+            </h1>
+            <Link href="#" type="pageButton">
+                Conheça meu trabalho
+            </Link>
         </section>
     )
 }
