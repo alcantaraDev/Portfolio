549e7e695d49e9ced9998a8f6f43056b15e39bce
Isaque Alcantara <alcantara.isaque167@gmail.com>
2023-05-12 12:32:53 -0300

    updated

--- components/sections/header/index.tsx
+++ components/sections/header/index.tsx
@@ -1,4 +1,3 @@
-import { BackgorundScroll } from "@/components/animate/backgroundScroll";
 import { Link } from "@/components/link";
 import { Logo } from "@/components/logo";
 import { MobileMenu, Menu, MenuButton, MenuLink } from "./mobileMenu";
@@ -9,26 +8,19 @@ export function Header() {
         className="
             w-screen h-[80px]
             fixed z-10
-        ">
-            <BackgorundScroll 
-            bgColor="#181818" 
-            offset={80} 
-            className="
             flex flex-row justify-between items-center
             px-[30px] xl:px-[100px]
-            "
-            >
-                <Logo type="name"/>
-                <MobileMenu>
-                    <MenuButton/>
-                    <Menu>
-                        <MenuLink href="#home">Home</MenuLink>
-                        <MenuLink href="#services">Serviços</MenuLink>
-                        <MenuLink href="#">Projetos</MenuLink>
-                        <MenuLink href="#contact" type="pageButton">Fale Comigo</MenuLink>
-                    </Menu>
-                </MobileMenu>  
-            </BackgorundScroll>
+        ">
+            <Logo type="name"/>
+            <MobileMenu>
+                <MenuButton/>
+                <Menu>
+                    <MenuLink href="#home">Home</MenuLink>
+                    <MenuLink href="#services">Serviços</MenuLink>
+                    <MenuLink href="#">Projetos</MenuLink>
+                    <MenuLink href="#contact" type="pageButton">Fale Comigo</MenuLink>
+                </Menu>
+            </MobileMenu>  
         </header>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,21 @@
-import { AnimationContainer } from "@/components/animate";
-import { BackGround } from "@/components/background";
-import { AbaoutMe } from "@/components/sections/aboutMe";
-import { ServicesSection } from "@/components/sections/services";
 import { Footer } from "@/components/sections/footer";
 import { HeadSection } from "@/components/sections/head";
 import { Header } from "@/components/sections/header";
 import { HomeSection } from "@/components/sections/home";
 
 import { Poppins } from "next/font/google"
-import { SocialMediaSection } from "@/components/sections/SocialMedia";
-import { ContactSection } from "@/components/sections/contact";
 
 const poppins = Poppins({ subsets: ["latin"], weight:["100", "200", "300", "400", "500", "600", "700", "800", "900"] })
  
 export default function Home() {
   return (
-    <AnimationContainer>
       <div className={poppins.className}>
         <HeadSection/>
-        {/* <BackGround/> */}
         <Header/>
         <main className="min-h-[calc(100vh-220px)]">
           <HomeSection/>
-          {/* <AbaoutMe/>
-          <ServicesSection/>
-          <SocialMediaSection/>
-          <ContactSection/> */}
         </main>
         <Footer/>
       </div>
-    </AnimationContainer>
   )
 }
